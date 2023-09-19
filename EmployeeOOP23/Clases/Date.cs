cedf5333adce4fac607b1b0fe1a9b4b18ffc02c6
pablo <pablo@DESKTOP-FRH24EN>
2023-09-18 22:45:36 -0500

    Proyect Creation - EmployeDate class createdeOOP

---
+++ EmployeeOOP23/Clases/Date.cs
@@ -0,0 +1,37 @@
+﻿namespace EmployeeOOP23.Clases
+{
+    public class Date
+    {
+        #region Fields
+
+        private int _year;
+        private int _month;
+        private int _day;
+
+        #endregion
+
+
+        #region Methods
+        public Date(int day, int month, int year)
+        {
+            _year = year;
+            _month = month;
+            _day = day;
+        }
+
+        public override string ToString()
+        {
+           //dia/mes/año
+           //var dateContenated1 = _day + "/" + _month + "/" + _year;
+           //var dateContenated2 = $"{_day}/{_month}/{_year}";
+           //var dateContenated3 = String.Format("{0}/{1}/{2}", _day, _month, _year);
+            return  String.Format("{0}/{1}/{2}", _day, _month, _year);
+
+           // return dateContenated;
+
+        }
+
+        #endregion
+
+    }
+}
--- EmployeeOOP23/Program.cs
+++ EmployeeOOP23/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using EmployeeOOP23.Clases;
+
+Console.WriteLine("OOP APPLICATION");
+Console.WriteLine("---------------");
+
+// Inicializacion de la clase date
+Date dateObject = new Date(4,5,565655);
+Console.WriteLine(dateObject.ToString());
+
